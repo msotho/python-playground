98809384895f2b20b7d88e24646a20e3ca403bbe
Sechaba Mofokeng <sechaba@onemoola.com>
2023-08-14 19:51:38 +0200

    refactor: functional strategy

--- design_patterns/strategy_functional_ex2.py
+++ design_patterns/strategy_functional_ex2.py
@@ -6,22 +6,22 @@ def sum_values(values: list[int], strategy: callable) -> int:
     return sum(map(strategy, values))
 
 
+def is_even(x: int) -> bool:
+    return x % 2 == 0
+
+
+def is_odd(x: int) -> bool:
+    return x % 2 != 0
+
+
+def square(x: int) -> int:
+    return x * x
+
+
 if __name__ == '__main__':
     numbers = [1, 2, 3, 4, 5]
 
-
-    def is_even(x: int) -> bool:
-        return x % 2 == 0
-
-
-    def is_odd(x: int) -> bool:
-        return x % 2 != 0
-
-
     print(sum_values_with_predicate(numbers, is_even))
     print(sum_values_with_predicate(numbers, is_odd))
-
-    def square(x: int) -> int:
-        return x * x
 
     print(sum_values(numbers, square))
