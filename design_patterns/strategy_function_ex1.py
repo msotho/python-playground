50f5a04c630593e8ee1fa73d4eb6ca0bdb1469f0
Sechaba Mofokeng <sechaba@onemoola.com>
2023-08-18 17:48:03 +0200

    refactor: how to call strategy

--- design_patterns/strategy_function_ex1.py
+++ design_patterns/strategy_function_ex1.py
@@ -2,17 +2,18 @@ def login(auth: callable, username: str, password: str) -> bool:
     return auth(username, password)
 
 
+def facebook_auth(username: str, password: str) -> bool:
+    print(f"Authenticating {username}:{password} with Facebook")
+
+    return True
+
+
+def google_auth(username: str, password: str) -> bool:
+    print(f"Authenticating {username}:{password} with Google")
+
+    return True
+
+
 if __name__ == "__main__":
-
-    def facebook_auth(username: str, password: str) -> bool:
-        print(f"Authenticating {username}:{password} with Facebook")
-
-        return True
-
-    def google_auth(username: str, password: str) -> bool:
-        print(f"Authenticating {username}:{password} with Google")
-
-        return True
-
     login(facebook_auth, username="user", password="pass")
     login(google_auth, username="user", password="pass")
